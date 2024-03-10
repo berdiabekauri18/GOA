4bdeb5af774bbe3f12f9b5aabf8594d27652f8d2
berdiabekauri18 <berdiagoasgit@gmail.com>
2024-03-10 13:33:05 +0400

    first commit

---
+++ day5/classwork/homework/hw.py
@@ -0,0 +1,42 @@
+#day 5 GOA homework
+
+weather = str(200)
+print(weather)
+
+city =" barcelona"
+print(city)
+
+rearrange = " numbers"
+print(rearrange)
+
+parentheses = " GOA"
+print(parentheses)
+
+variety = " people"
+print(variety)
+
+incorrect = " parentheses are missing"
+print(incorrect)
+
+loss = " game over"
+print(loss)
+
+select = " ronaldo"
+print(select)
+
+handle = " cup"
+print(handle)
+
+tell =" a secret"
+print(tell)
+
+survey = " children"
+print(survey)
+
+similar = " glasses"
+print(similar)
+
+explicit = " lines"
+print(explicit)
+
+print(weather + city + rearrange + parentheses + variety + incorrect + loss + select + handle + tell + survey + similar + explicit)
---
+++ day6/homework/hw.py
@@ -0,0 +1,38 @@
+#day 6 GOA homework
+
+#homework 1
+
+name = "nika"
+last_name = "nutsubize"
+age = 14
+live = "saburtalo"
+love = "programing"
+print(name, last_name, age, live, love)
+
+#name = input("enter your name:")
+#last_name = input("enter your last name:")
+#age = input("enter your age:")
+#live = input("where do you live:")
+#love = input("what do you love:")
+#print(name, last_name, age, live, love)
+
+#homework 2
+
+print(69 + 106)
+print(69 - 106)
+print(69 * 106)
+print(69 // 106)
+
+#homework 3
+
+#my moms and dads age this year
+
+moms_age = 42
+dads_age = 46
+print(moms_age, dads_age)
+
+#my moms and dads age after 25 years
+
+moms_age = 67
+dads_age = 71
+print(moms_age, dads_age)
