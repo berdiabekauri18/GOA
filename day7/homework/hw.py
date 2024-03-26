74c65b0b08ab37c4e324f989b8e4b349e4c2a7b8
berdiabekauri18 <161619481+berdiabekauri18@users.noreply.github.com>
2024-03-26 19:27:13 +0400

    fourth commit

---
+++ day7/homework/hw.py
@@ -0,0 +1,84 @@
+#day 7 Goa homework
+
+#homework 1: tell your helper 3 diffrent types
+
+#int
+num = 27
+print(type(num))
+
+#float
+number = 2.5
+print(type(number))
+
+#str
+name = "berdia"
+print(type(name))
+
+#homework 2: tell your helper 5 diffrent numbers and use +,-,*,/ 10 times in diffrent ways. numbers:14, 15, 20, 69, 17
+
+print(14 + 15 + 20 + 69 + 17)
+print(69 - 20 - 17 - 15 - 14)
+print(14 * 15 * 20 * 69 * 17)
+print(69 // 20 // 17 // 15 // 14)
+
+x = 14
+y = 15
+z = 20
+a = 69
+b = 17
+print(x + y + z + a + b)
+
+x = 69
+y = 20
+z = 17
+a = 15
+b = 14
+print(x - y - z - a - b)
+
+x = 14
+y = 15
+z = 20
+a = 69
+b = 17
+print(x * y * z * a * b)
+
+x = 69
+y = 20
+z = 17
+a = 15
+b = 14
+print(x // y // z // a // b)
+
+x = 14
+y = 15
+z = 20
+a = 69
+b = 17
+print(x + y - z * a // b)
+
+x = 69
+y = 20
+z = 17
+a = 15
+b = 14
+print(x // y * z - a + b)
+
+#homework 3 : make diffrent types by your own to learn how to use type()
+
+name = "berdia"
+print(type(name))
+
+age = 11
+print(type(age))
+
+float_num = float(3)
+print(type(float_num))
+
+normal_text = str(True)
+print(type(normal_text))
+
+int_num = int("27")
+print(type(int_num))
+
+float_number = float(3)
+print(type(float_number))
